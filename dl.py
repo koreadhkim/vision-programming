6679cbc9027bf33e3dce2ee9e4c393f08deaa4d2
Daehan Kim <48986889+koreadhkim@users.noreply.github.com>
2020-09-13 02:06:36 +0900

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,61 +1,52 @@
 import torch
 import torch.nn as nn
-import torchvision
-import torch.backends.cudnn as cudnn
-import torch.optim
-import os
-import sys
-import argparse
-import time
-import dataloader
-import model
+import torch.nn.functional as F
+import math
+#import pytorch_colors as colors
 import numpy as np
-from torchvision import transforms
-from PIL import Image
-import glob
-import time
 
+class enhance_net_nopool(nn.Module):
+	def __init__(self):
+		super(enhance_net_nopool, self).__init__()
+		self.relu = nn.ReLU(inplace=True)
+		number_f = 32
+		self.e_conv1 = nn.Conv2d(3,number_f,3,1,1,bias=True)
+		self.e_conv2 = nn.Conv2d(number_f,number_f,3,1,1,bias=True)
+		self.e_conv3 = nn.Conv2d(number_f,number_f,3,1,1,bias=True)
+		self.e_conv4 = nn.Conv2d(number_f,number_f,3,1,1,bias=True)
+		self.e_conv5 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True)
+		self.e_conv6 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True)
+		self.e_conv7 = nn.Conv2d(number_f*2,24,3,1,1,bias=True)
 
+                self.maxpool = nn.MaxPool2d(2, stride=2, return_indices=False, ceil_mode=False)
+                self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+                self.Minseock_ffc_01 = LayerNorm(number_f)
+        	self.Minseock_ffc_02 = LayerNorm(number_f)
+                self.Minseock_ffc_03 = LayerNorm(number_f)
+		self.Minseock_ffc_04 = LayerNorm(number_f)
+		self.Minseock_ffc_05 = LayerNorm(number_f)
+		self.Minseock_ffc_06 = LayerNorm(number_f)
+		self.Minseock_ffc_07 = LayerNorm(number_f)
+		self.Minseock_ffc_08 = LayerNorm(number_f)
 
-def lowlight(image_path):
-	os.environ['CUDA_VISIBLE_DEVICES']='0'
-	data_lowlight = Image.open(image_path)
+        def forward(self, x):
 
- 
+                x1 = self.relu(self.e_conv1(x))
+                # p1 = self.maxpool(x1)
+                x2 = self.relu(self.e_conv2(x1))
+                # p2 = self.maxpool(x2)
+                x3 = self.relu(self.e_conv3(x2))
+                # p3 = self.maxpool(x3)
+                x4 = self.relu(self.e_conv4(x3))
 
-	data_lowlight = (np.asarray(data_lowlight)/255.0)
+                x5 = self.relu(self.e_conv5(torch.cat([x3,x4],1)))
+                # x5 = self.upsample(x5)
+                x6 = self.relu(self.e_conv6(torch.cat([x2,x5],1)))
 
+                x_r = F.tanh(self.e_conv7(torch.cat([x1,x6],1)))
+                r1,r2,r3,r4,r5,r6,r7,r8 = torch.split(x_r, 3, dim=1)
 
-	data_lowlight = torch.from_numpy(data_lowlight).float()
-	data_lowlight = data_lowlight.permute(2,0,1)
-	data_lowlight = data_lowlight.cuda().unsqueeze(0)
-	data_lowlight = torch.nn.functional.interpolate(data_lowlight,(1080,1920),mode='bilinear')
-	DCE_net = model.enhance_net_nopool().cuda()
-	DCE_net.load_state_dict(torch.load('snapshots/Epoch99.pth'))
-	start = time.time()
-	_,enhanced_image,_ = DCE_net(data_lowlight)
-
-	end_time = (time.time() - start)
-	print(end_time)
-	image_path = image_path.replace('test_data','result')
-	result_path = image_path
-	if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
-		os.makedirs(image_path.replace('/'+image_path.split("/")[-1],''))
-
-	torchvision.utils.save_image(enhanced_image, result_path)
-
-if __name__ == '__main__':
-# test_images
-	with torch.no_grad():
-		filePath = 'data/test_data/'
-	
-		file_list = os.listdir(filePath)
-
-		for file_name in file_list:
-			test_list = glob.glob(filePath+file_name+"/*") 
-			for image in test_list:
-				# image = image
-				print(image)
-				lowlight(image)
-
-		
+                x = x + r1*(torch.pow(x,2)-x)
+                x = x + r2*(torch.pow(x,2)-x)
+                x = x + r3*(torch.pow(x,2)-x)
+                return enhance_image_1,enhance_image,r
